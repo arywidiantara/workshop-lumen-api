d417479bc020caf94af34279e627535d5646bf85
arywidiantara <arywidiantara33@gmail.com>
2017-12-14 07:02:36 +0700

    [add] set middleware

--- app/Http/Middleware/ExampleMiddleware.php
+++ app/Http/Middleware/ExampleMiddleware.php
@@ -9,12 +9,16 @@ class ExampleMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
+        if ($request->header('key') != env('APP_KEY'))
+        {
+            return response()->json(['key invalid'], 400);
+        }
         return $next($request);
     }
 }
